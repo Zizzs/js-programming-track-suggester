a81e8ca5a80d2afb5761baaf563f98a08c9271d8
Alex Williams <Zizzs17@gmail.com>
2018-10-19 09:53:13 -0700

    Added CSS Program as a choice. Added Front/Backend form choice.

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,41 @@ $(document).ready(function(event) {
         event.preventDefault();
         var company = parseInt($("#companySize").val());
         var money = $("input:radio[name=money]:checked").val();
+        var frontEndBackEnd = parseInt($("#frontBackend").val());
+        
 
 
-        if (company === 1 && money === "moneyOne") {
+        if (company === 1 && 
+            money   === "moneyOne" &&
+            frontEndBackEnd === 2 ||
+            frontEndBackEnd === !1) {
             result = "C#";
-        } else if (company === 2 && money === "moneyOne") {
-            result = "Java";
-        } else if (company === 3 && money === "moneyTwo") {
-            result = "PHP";
+
+        } else if (company === 2 ||
+                   company === 3 && 
+                   money === "moneyOne" &&
+                   frontEndBackEnd === 2) {
+                   result = "Java";
+
+        } else if (company === 1 || 
+                   company === 2 || 
+                   company === 3 || 
+                   company === 4 && 
+                   money === "moneyOne" || 
+                   money === "moneyTwo" && 
+                   frontEndBackEnd === 1 ||
+                   frontEndBackEnd === !2) {
+                   result = "CSS";
+
+        } else if (company === 3 && 
+                   money === "moneyTwo") {
+                   result = "PHP";
+
         } else if (money === "moneyTwo") {
-            result = "Ruby";
-        } else if (money === "moneyThree") {
-            result = "Well, sorry to burst your bubble boy-o. You'll most likely be making money as a programmer. Tough Luck."
+                   result = "Ruby";
+
+        // } else if (money === "moneyThree") {
+        //       result = "Well, sorry to burst your bubble boy-o. You'll most likely be making money as a programmer. Tough Luck."
         }
 
         if (result === "C#") {
@@ -24,6 +47,7 @@ $(document).ready(function(event) {
             $("#phpInfo").hide();
             $("#rubyInfo").hide();
             $("#noInfo").hide();
+            $("#cssInfo").hide();
         }
         if (result === "Java") {
             $("#csharpInfo").hide();
@@ -31,6 +55,7 @@ $(document).ready(function(event) {
             $("#phpInfo").hide();
             $("#rubyInfo").hide();
             $("#noInfo").hide();
+            $("#cssInfo").hide();
         }
         if (result === "PHP") {
             $("#csharpInfo").hide();
@@ -38,6 +63,7 @@ $(document).ready(function(event) {
             $("#phpInfo").show();
             $("#rubyInfo").hide();
             $("#noInfo").hide();
+            $("#cssInfo").hide();
         }
         if (result === "Ruby") {
             $("#csharpInfo").hide();
@@ -45,14 +71,24 @@ $(document).ready(function(event) {
             $("#phpInfo").hide();
             $("#rubyInfo").show();
             $("#noInfo").hide();
+            $("#cssInfo").hide();
         }
-        if (result = "Well, sorry to burst your bubble boy-o. You'll most likely be making money as a programmer. Tough Luck.") {
+        if (result === "CSS") {
             $("#csharpInfo").hide();
             $("#javaInfo").hide();
             $("#phpInfo").hide();
             $("#rubyInfo").hide();
-            $("#noInfo").show();
+            $("#noInfo").hide();
+            $("#cssInfo").show();
         }
+        // if (result = "Well, sorry to burst your bubble boy-o. You'll most likely be making money as a programmer. Tough Luck.") {
+        //     $("#csharpInfo").hide();
+        //     $("#javaInfo").hide();
+        //     $("#phpInfo").hide();
+        //     $("#rubyInfo").hide();
+        //     $("#noInfo").show();
+        //     $("#cssInfo").hide();
+        // }
 
 
         $("#output").text(result);
